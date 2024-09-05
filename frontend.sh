33071f9d8a06c163bef0e6356c17fad0d321aaee
Amarnathg <amarnath.gotru4@gmail.com>
2024-09-05 15:41:04 +0530

    expense project-backend

---
+++ frontend.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # $0--> will gives us script name.
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+USERID=$(id -u)
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+       echo -e"$R please run this script with sudo previleges $N" | tee -a &>>$LOG_FILE
+       exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+      echo -e " $2 is $R failed.. $N" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo -e " $2 is $G success. $N"  | tee -a $LOG_FILE
+    fi
+}
+
+echo "script started executing at :$(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Starting of Nginx"
+
+#Remove the default website
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default website"
+
+#Download the frontend content
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading of frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting the front end code"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx
+
